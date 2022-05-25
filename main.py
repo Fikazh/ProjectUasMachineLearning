633c401f8638d92bc4cff87d7e59d04152d6cbe2
fikri azhari <fikriazhari551@gmail.com>
2022-05-25 19:54:29 +0700

    ganti datafield

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+def readData(path):
+    data = pd.read_csv(path)
+    return data
+
+
+def __main__():
+    path = "german_credit_data.csv"
+    df = readData(path)
+
+    # Check Datafield
+    print("Datafield\n", df.head, "\n")
+    print("Jumlah data dan fitur\n", df.shape, "\n")
+    print("Jenis Data\n", df.dtypes, "\n")
+    print(df.info)
+    df.replace("?", np.nan, inplace=True)
+    print(df.isnull().sum())
+    print(df.isnull().any())
+    print(df.dropna)
+
+
+__main__()
